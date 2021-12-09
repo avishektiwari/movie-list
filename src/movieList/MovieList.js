f99ceb96a518e7123e6c339d2691f5904fb7d1e1
Avishek <avishektiwari@lftechnology.com>
2021-12-09 10:38:13 +0545

    First commit, Finish setting up and initiating app.

---
+++ src/movieList/MovieList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./../logo.svg";
+import "./MovieList.css";
+import MovieCard from "./components/MovieCard";
+
+const MovieList = () => {
+  return (
+    <div className="background">
+      <MovieCard />
+    </div>
+  );
+};
+
+export default MovieList;
---
+++ src/movieList/components/MovieCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MovieCard = () => {
+  return <div>THis is div</div>;
+};
+
+export default MovieCard;
