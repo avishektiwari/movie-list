6aa968634fe90c25669899af55db844d0bb3d829
Avishek <avishektiwari@lftechnology.com>
2021-12-10 12:51:53 +0545

    Integrate API into the app.

--- src/movieList/MovieList.js
+++ src/movieList/MovieList.js
@@ -1,29 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./MovieList.css";
 import "./components/MovieCard.css";
 import MovieCard from "./components/MovieCard";
 import { Grid } from "@material-ui/core";
 
+import * as services from "../services/http";
+
 const MovieList = () => {
+  const [trendingList, seTrendingList] = useState([]);
+  const [genres, setGenres] = useState([]);
   const movie = [
-    { title: "This", subtitle: "that" },
-    { title: "This", subtitle: "that" },
-    { title: "This", subtitle: "that" },
+    { name: "This", overview: "that" },
+    { name: "This", overview: "that" },
+    { name: "Thisis", overview: "that" },
   ];
+
+  async function fetchTrendingList() {
+    const movieGenres = await services.fetchMovieGenre();
+    const tvGenres = await services.fetchTVGenre();
+    const genres = movieGenres.concat(tvGenres);
+
+    const data = await services.fetchMovieList();
+
+    setGenres(genres);
+    const movies = data.map((item) => {
+      const genre = item.genre_ids.map((g) => {
+        return genres.find((t) => t.id == g);
+      });
+      return { ...item, genre };
+    });
+    seTrendingList(movies);
+  }
+
+  useEffect(() => {
+    fetchTrendingList();
+  }, []);
+
+  const movieList = trendingList.filter((item) => item.media_type === "movie");
+  const tvList = trendingList.filter((item) => item.media_type === "tv");
+
   return (
     <div className="background">
       <Grid container className="grid-container">
         <Grid sm={6}>
           <span className="header">Trending TV Shows</span>
-          {movie.map((item) => (
-            <MovieCard title={item.title} subtitle={item.subtitle} />
+          {movieList.map((item) => (
+            <MovieCard
+              title={item.title}
+              id={item.id}
+              voteAverage={item.vote_average}
+              posterPath={item.backdrop_path}
+              mediaType={item.media_type}
+              genre={item.genre}
+            />
           ))}
         </Grid>
         <Grid sm={6}>
           <span className="header">Trending Movies</span>
-          {movie.map((item) => (
-            <MovieCard title={item.title} subtitle={item.subtitle} />
+          {tvList.map((item) => (
+            <MovieCard
+              title={item.name}
+              id={item.id}
+              voteAverage={item.vote_average}
+              posterPath={item.backdrop_path}
+              mediaType={item.media_type}
+              genre={item.genre}
+            />
           ))}
         </Grid>
       </Grid>
--- src/movieList/components/MovieCard.js
+++ src/movieList/components/MovieCard.js
@@ -1,24 +1,64 @@
 import React from "react";
-
-import "./MovieCard.css";
 import { Card } from "@material-ui/core";
 
+import "./MovieCard.css";
+import * as config from "../../config";
+import { fetchMovie, fetchTVShows } from "../../services/http";
+
 const MovieCard = (props) => {
-  const { title, subtitle } = props;
+  const { title, voteAverage, posterPath, id, mediaType, genre } = props;
+
+  const imageUrl = config.imageBaseURL + posterPath;
+
+  async function handleTitleOnClick(id) {
+    if (mediaType === "movie") {
+      const data = await fetchMovie(id);
+      window.open(config.imdbTitleURL + data.imdb_id);
+    } else if (mediaType === "tv") {
+      const data = await fetchTVShows(id);
+      window.open(data.homepage);
+    }
+  }
+
+  async function handleTrailerOnClick() {
+    const youtubeSearch = new URL(config.youtube);
+    youtubeSearch.searchParams.set("search_query", title);
+    window.open(youtubeSearch);
+  }
+
   return (
     <Card className="card" style={{ backgroundColor: "#636060" }}>
-      <img className="thumbnail" src="https://i.imgur.com/LdV1hb0.png" />
+      <img
+        className="thumbnail hover-pointer"
+        onClick={() => handleTitleOnClick(id)}
+        src={imageUrl || "https://i.imgur.com/LdV1hb0.png"}
+      />
       <div className="info">
         <div>
-          <span className="header">10</span>
+          <span className="header">{voteAverage}</span>
           <span className="subheader">/10</span>
         </div>
-        <div>
-          <span className="header mr-8">{title}</span>
-          <span className="minitext">Genre.</span>
+        <div className="flex">
+          <span
+            className="header mr-8 hover-pointer"
+            onClick={() => handleTitleOnClick(id)}
+          >
+            {title}
+          </span>
+          <span className="minitext">
+            <ul className="horizontal">
+              {genre.map((g) => (
+                <li>{g.name}</li>
+              ))}
+            </ul>
+          </span>
         </div>
-        <div className="minitext pv-1">{subtitle}</div>
-        <div className="subheader pv-1">Watch Trailer</div>
+        <div
+          className="subheader pv-1 hover-pointer"
+          onClick={() => handleTrailerOnClick()}
+        >
+          Watch Trailer
+        </div>
       </div>
     </Card>
   );
